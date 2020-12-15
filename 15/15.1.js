d29ecb433f894407dce986e8b1136f5e4503faeb
Carly Huang <c85615@gmail.com>
2020-12-14 22:52:59 -0800

    add day 15 answer

---
+++ 15/15.1.js
@@ -0,0 +1,24 @@
+const starting_nums = [18,8,0,5,4,1,20]
+
+function getithNumber(i) {
+    const cache = {}
+    let number = null
+    for (let turn = 1; turn < starting_nums.length; turn++) {
+        number = starting_nums[turn - 1]
+        cache[number] = turn
+    }
+    number = starting_nums[starting_nums.length - 1]
+    for (turn = starting_nums.length + 1; turn <= i; turn++) {
+        if (!cache.hasOwnProperty(number)) {
+            cache[number] = turn - 1
+            number = 0
+        } else {
+            const last_number = number
+            number = turn - 1 - cache[number]
+            cache[last_number] = turn - 1
+        }
+    }
+    return number
+}
+
+console.log("2020th number: ", getithNumber(2020))
